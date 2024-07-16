14dac48263d58785242a2506606b7dbf29002b63
usama <usamahafeez948@gmail.com>
2024-07-16 18:29:24 +0500

    ...

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -27,7 +27,7 @@ export class AppController {
         httpOnly: false
       })
       // res.setHeader("Access-Control-Allow-Origin", "*")
-      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
       res.setHeader("Access-Control-Allow-Credentials", "true");
       res.setHeader("Access-Control-Max-Age", "1800");
       res.setHeader("Access-Control-Allow-Headers", "content-type");
