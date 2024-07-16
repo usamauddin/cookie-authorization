c5acd799f7a809b0d8a362e2bebc4aa22bfa1f29
usama <usamahafeez948@gmail.com>
2024-07-16 18:27:13 +0500

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
-  app.enableCors({
-    origin: "http://localhost:3000",
-    methods: "GET,PUT,PATCH,POST,DELETE",
+  // app.enableCors({
+  //   origin: "http://localhost:3000",
+  //   methods: "GET,PUT,PATCH,POST,DELETE",
+  //   credentials: true,
+  //   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin']
+  // })
+  const corsOrigin = {
+    origin: 'http://localhost:3000', //or whatever port your frontend is using
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin']
-  })
+    optionSuccessStatus: 200
+  }
+  app.use(cors(corsOrigin));
   await app.listen(3000);
   console.log('server running at 3000');
-  
+
 }
 bootstrap();
