eea02a5b55a8e57b2a05a84497ebff992786f7cb
usama <usamahafeez948@gmail.com>
2024-07-16 18:17:04 +0500

    ...

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true
+    origin: "http://localhost:3000",
+    methods: "GET,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin']
   })
   await app.listen(3000);
   console.log('server running at 3000');
