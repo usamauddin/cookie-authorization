7102d99675b1b24ab357eaeea82d45ccb338e079
usama <usamahafeez948@gmail.com>
2024-07-23 13:59:43 +0500

    ..

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -22,7 +22,7 @@ export class AppController {
       res.cookie('cookie', encrypted, {
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
         sameSite: 'none',
-        // secure: true,
+        secure: true,
         httpOnly: true,
         path: '/'
       })
